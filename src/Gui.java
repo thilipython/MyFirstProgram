a750a7f8bdd87743070f582a2a5fd1a77726986a
thilipython <karoltharaka@gmail.com>
2024-02-11 20:59:40 +0530

    updates

--- src/Gui.java
+++ src/Gui.java
@@ -6,6 +6,12 @@ public class Gui {
 		
 		String name =JOptionPane.showInputDialog("What is ur name");
 		JOptionPane.showMessageDialog(null, "Hello "+name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("What is ur age"));
+		JOptionPane.showMessageDialog(null, "Your age is "+age);
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("What is ur height"));
+		JOptionPane.showMessageDialog(null, "Your age is "+height);
 	}
 
 }
